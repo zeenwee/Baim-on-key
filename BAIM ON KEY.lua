37d1973501511c311dc8b5d456cb019712668dd6
zeenwee <68040236+zeenwee@users.noreply.github.com>
2021-01-15 13:35:33 +0000

    uadate
    update

--- BAIM ON KEY.lua
+++ BAIM ON KEY.lua
@@ -1,69 +1,86 @@
 --local author = "zeenwee";
 --https://github.com/zeenwee/Baim-on-key/blob/master/BAIM%20ON%20KEY.lua  
-local ref = gui.Tab(gui.Reference("Misc"), "baimgroup", "Baim on key")
-local baimgroup = gui.Groupbox(ref, "Baim on key", 10, 10, 200, 200);
-local keyset = gui.Keybox(baimgroup, "keyset", "key", 86);
-keyset:SetDescription("toggle to switch the baim and lethal")
-function baimtokey()
-        if input.IsButtonPressed(keyset:GetValue()) then
-            pressed=true;
-            Toggle = Toggle *-1
+local group = gui.Groupbox( gui.Reference( 'Ragebot', 'Hitscan' ), 'Baim on key', 328, 311, 296, 200 )
+local mode = gui.Combobox( group, 'baim_on_key_mode', 'Mode', 'Toggle', 'Hold' )
+local hotkey = gui.Keybox( group, 'baim_on_key_key', 'Key', 86 )
+hotkey:SetDescription( 'Switches between baim and lethal.' )
 
-        elseif (pressed and input.IsButtonReleased(keyset:GetValue())) then
-            pressed=false;
+local font = draw.CreateFont( 'Tahoma', 24, 1000 )
+local screenW, screenH = draw.GetScreenSize()
+local X, Y = 10, screenH * 0.6
 
-            if Toggle == 1 then
-        		 gui.Command('rbot.hitscan.mode.pistol.bodyaim.lethal "Off"' )
-				 gui.Command('rbot.hitscan.mode.hpistol.bodyaim.lethal "Off"' )
-				 gui.Command('rbot.hitscan.mode.scout.bodyaim.lethal "Off"' )
-				 gui.Command('rbot.hitscan.mode.asniper.bodyaim.lethal "Off"' )
-				 gui.Command('rbot.hitscan.mode.sniper.bodyaim.lethal "Off"' )
-				 gui.Command('rbot.hitscan.mode.shared.bodyaim.lethal "Off"' )
-                 gui.Command('rbot.hitscan.mode.pistol.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.pistol.forcesafe", true )
-                 gui.Command('rbot.hitscan.mode.hpistol.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.hpistol.forcesafe", true )
-				 gui.Command('rbot.hitscan.mode.scout.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.scout.forcesafe", true )
-				 gui.Command('rbot.hitscan.mode.sniper.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.sniper.forcesafe", true )
-				 gui.Command('rbot.hitscan.mode.asniper.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.asniper.forcesafe", true )
-				 gui.Command('rbot.hitscan.mode.shared.bodyaim.force "On"' )
-                 guiSet("rbot.hitscan.mode.shared.forcesafe", true )
-            else
-                 gui.Command('rbot.hitscan.mode.pistol.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.pistol.forcesafe", false )
-                 gui.Command('rbot.hitscan.mode.hpistol.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.hpistol.forcesafe", false )
-				 gui.Command('rbot.hitscan.mode.scout.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.scout.forcesafe", false )
-				 gui.Command('rbot.hitscan.mode.sniper.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.sniper.forcesafe", false )
-				 gui.Command('rbot.hitscan.mode.asniper.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.asniper.forcesafe", false )
-				 gui.Command('rbot.hitscan.mode.shared.bodyaim.force "Off"' )
-                 guiSet("rbot.hitscan.mode.shared.forcesafe", false )				 
-				 gui.Command('rbot.hitscan.mode.pistol.bodyaim.lethal "On"' )
-				 gui.Command('rbot.hitscan.mode.hpistol.bodyaim.lethal "On"' )
-				 gui.Command('rbot.hitscan.mode.scout.bodyaim.lethal "On"' )
-				 gui.Command('rbot.hitscan.mode.asniper.bodyaim.lethal "On"' )
-				 gui.Command('rbot.hitscan.mode.sniper.bodyaim.lethal "On"' )
-				 gui.Command('rbot.hitscan.mode.shared.bodyaim.lethal "On"' )
+local types = { 'shared.', 'zeus.', 'pistol.', 'hpistol.', 'smg.', 'rifle.', 'shotgun.', 'scout.', 'asniper.', 'sniper.', 'lmg.' }
+local active = { [true]={['bodyaim.force']=1, ['bodyaim.lethal']=0, ['bodyaim.onshot']=0, ['bodyaim.safepoint']=0, ['prefersafe']=0, ['forcesafe']=1, ['forcesafe.limbs']=1, ['forcesafe.head']=1, ['forcesafe.body']=1}, [false]={} }
 
-            end
-        end
+local function setValues( val )
+for i=1, #types do
+local wep = 'rbot.hitscan.mode.' .. types[i]
+
+if val then
+for k, v in pairs( active[true] ) do
+active[false][ wep .. k ] = gui.GetValue( wep .. k )
+gui.SetValue( wep .. k, v )
 end
-callbacks.Register("Draw", "baimtokey", baimtokey);
-callbacks.Register( "Draw", function()
-  if not entities.GetLocalPlayer() or not entities.GetLocalPlayer():IsAlive() then
- return end
-   if gui.GetValue( "rbot.hitscan.mode.pistol.forcesafe" ) then
-      draw.Color(112, 255, 0)   
-   else
-      draw.Color(255, 25, 25)
-   end
-   local screenW, screenH = draw.GetScreenSize()
-      draw.TextShadow(10, screenH - 500, "BAIM" )
+else
+for k, v in pairs( active[false] ) do
+gui.SetValue( k, v )
+end
+end
+end
+end
 
-end )
+local function move_box()
+for i=1, #types do
+if gui.GetValue( 'rbot.hitscan.mode.'.. types[i] ..'forcesafe' ) then
+return true
+end
+end
+end
+
+local pressed = false
+callbacks.Register( 'Draw', function()
+if move_box() then
+group:SetPosY( 366 )
+else
+group:SetPosY( 311 )
+end
+
+local key = hotkey:GetValue()
+if key == 0 then
+return
+end
+
+if not entities.GetLocalPlayer() then
+return
+end
+
+local mode = mode:GetString()
+if mode == 'Hold' then
+if input.IsButtonDown( key ) then
+if not pressed then
+setValues( true )
+pressed = true
+end
+else
+if pressed then
+setValues( false )
+pressed = false
+end
+end
+
+elseif mode == 'Toggle' then
+if input.IsButtonReleased( key ) then
+pressed = not pressed
+setValues( pressed )
+end
+end
+
+local r, g, b = 255, 25, 25
+if pressed then
+r, g, b = 112, 255, 0
+end
+
+draw.SetFont( font )
+draw.Color( r, g, b )
+draw.TextShadow( X, Y, 'BAIM' )
+end)
